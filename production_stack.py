1c10f38ebc4fe1c098a4e274112696e604a8ebc5
marioaieie <marioaieie@gmail.com>
2019-06-05 23:24:39 +0200

    First production stack

--- production_stack.py
+++ production_stack.py
@@ -1,7 +1,35 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-sources_names = ["Clean coal", "Nuclear", "Tide", "Wave", "Hydro", "Waste", "Pumped heat", "Wood", "Solar HW",
-                 "Biofuels", "Photovoltaic", "Wind", "Solar in deserts", ]
-sources = pd.DataFrame(index=sources_names)
+# sources_names = ["Clean coal", "Nuclear", "Tide", "Wave", "Hydro", "Waste", "Pumped heat", "Wood", "Solar HW",
+#                  "Biofuels", "Photovoltaic", "Wind", "Solar in deserts", ]
+sources = pd.read_csv('sources.csv')
+sources.sort_values('source', inplace=True)
 
-print(sources.head())
+sources['area'] = 44
+sources['power'] = sources['power_density'] * sources['area'] / 1000. * 24
+
+# sources['power'] = 20
+# sources['area'] = sources['power'] / sources['power_density'] * 1000 / 24.
+
+sources['cumulative_power'] = sources['power'].cumsum()
+
+print(sources)
+print(sources['power'].sum())
+
+# plt.stackplot([0, 1], np.array([sources['power'], sources['power']]).T, alpha=0.4, labels=sources['source'])
+# plt.legend()
+
+# plt.bar(0, height=sources['power'],
+#         bottom=sources['cumulative_power'] - sources['power'], color='C' + sources.index.astype(str))
+
+for idx, item in sources.iterrows():
+    plt.bar(0, height=item['power'],
+            bottom=item['cumulative_power']-item['power'], alpha=0.4)
+    plt.text(0, item['cumulative_power'] - item['power']/2.,
+             '{name}: {pow:0.1f} kWh/d'.format(name=item['source'], pow=item['power']),
+             horizontalalignment='center', verticalalignment='center')
+
+plt.show()
+
